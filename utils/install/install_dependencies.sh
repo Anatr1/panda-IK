2ea349a7a3320b468f6a8ffd5d4e76766169d21f
Anatr1 <fedemustik@gmail.com>
2023-11-29 14:47:13 +0100

    removed unused libraries on CMakeLists.txt

--- utils/install/install_dependencies.sh
+++ utils/install/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-sudo apt install build-essential doxygen -y
+#sudo apt install build-essential doxygen -y
 sudo apt-get install libeigen3-dev -y
 
 # DIANA-Template Cpp - 2023
